d8dd2d552703f5262de8894054d9ff1e6b180886
kshitijdesai99 <79054156+kshitijdesai99@users.noreply.github.com>
2021-08-30 17:57:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let random1 = Math.floor(Math.random()*6)+1;
+let randomDiceImage = "dice"+random1+".png";
+let randomSorceImg = "images/"+randomDiceImage;
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomSorceImg);
+
+let random2 = Math.floor(Math.random()*6)+1;
+let randomDiceImage2 = "dice"+random2+".png";
+let randomSorceImg2 = "images/"+randomDiceImage2;
+document.querySelectorAll("img")[1].setAttribute("src",randomSorceImg2);
+
+
+// if player1 wins
+if (random1>random2){
+  document.querySelector("h1").innerHTML = " <i class='fas fa-flag'></i> <i>Player</i> <i>1</i> <i>Wins</i>";
+
+}
+
+else if(random2>random1)
+{
+  document.querySelector("h1").innerHTML = " <i>Player</i> <i>2</i> <i>Wins</i> <i class='fas fa-flag'></i>";
+
+}
+
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
